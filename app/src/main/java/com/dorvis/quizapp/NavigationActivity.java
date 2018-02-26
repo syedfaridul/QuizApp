e3b75296c117f1d7091335d37f7fe45fcc22bd50
Sainathhiwale <sainathhiwale2@gmail.com>
2018-02-26 23:21:30 +0530

    test updated

--- app/src/main/java/com/dorvis/quizapp/NavigationActivity.java
+++ app/src/main/java/com/dorvis/quizapp/NavigationActivity.java
@@ -12,6 +12,7 @@ import android.support.annotation.NonNull;
 import android.support.v4.app.FragmentManager;
 
 
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.support.design.widget.NavigationView;
@@ -23,16 +24,16 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.dorvis.quizapp.authentication.LoginActivity;
+
 import com.dorvis.quizapp.fragments.FirstHomeFragment;
-import com.dorvis.quizapp.fragments.ToDoNotificationFragment;
+import com.dorvis.quizapp.sql.SessionManager;
 
 import java.io.File;
 
 
 public class NavigationActivity extends AppCompatActivity {
-
-
+    private static final int SELECT_PHOTO = 100;
+    private SessionManager sessionManager;
     DrawerLayout mDrawerLayout;
     NavigationView mNavigationView;
     ImageView imageView;
@@ -43,8 +44,12 @@ public class NavigationActivity extends AppCompatActivity {
     TextView username_txtview;
     // header.xml action bar item
     ImageView signin_img;
+    TextView textViewName;
+
     //navigation menu floder
     ImageView imageView_Share;
+    ImageView imageView_navsend;
+    ImageView imageView_navNotification;
 
 
     //home fragment imageview
@@ -55,20 +60,18 @@ public class NavigationActivity extends AppCompatActivity {
         setContentView(R.layout.activity_navigation);
 
 
-        signin_img = (ImageView) findViewById(R.id.header_signin);
-        username_txtview = (TextView) findViewById(R.id.userName_txtview);
-        profile_imgView = (ImageView) findViewById(R.id.imageView_profile);
+       initViews();
 
-        signin_img.setOnClickListener(new View.OnClickListener() {
+
+
+
+        imageView_Share = (ImageView) findViewById(R.id.header_share);
+        imageView_Share.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(NavigationActivity.this, LoginActivity.class);
-                startActivity(intent);
-
+                showAlertDialog();
             }
         });
-
-
 
 
         FragmentManager manager = this.getSupportFragmentManager();
@@ -95,15 +98,23 @@ public class NavigationActivity extends AppCompatActivity {
                     startActivity(new Intent(NavigationActivity.this, NewsActivity.class));
 
                 } else if (id == R.id.nav_notification) {
-                    startActivity(new Intent(NavigationActivity.this, ToDoNotificationFragment.class));
-                }else if (id == R.id.nav_share){
 
+
+
+                }else if (id == R.id.nav_share){
+                    showAlertDialog();
+
+                }else if (id == R.id.nav_send){
+                    showContactUsDialog();
+                }else if (id == R.id.header_signout){
+                    logout();
                 }
 
 
                 return false;
             }
         });
+
 
 
         android.support.v7.widget.Toolbar toolbar = (android.support.v7.widget.Toolbar) findViewById(R.id.toolbar);
@@ -117,5 +128,90 @@ public class NavigationActivity extends AppCompatActivity {
 
     }
 
+    private void logout() {
+        sessionManager.setLoggedin(true);
+        finish();
+        startActivity(new Intent(NavigationActivity.this,LoginActivity.class));
+
+
+    }
+
+    private void showAlertDialog() {
+      new AlertDialog.Builder(this)
+              .setMessage("Choose action for share this app...")
+              .setPositiveButton("BLUETOOTH", new DialogInterface.OnClickListener() {
+                  @Override
+                  public void onClick(DialogInterface dialog, int which) {
+                      shareApplicationByBluetooth();
+                  }
+              }).setNegativeButton("OTHERS", new DialogInterface.OnClickListener() {
+          @Override
+          public void onClick(DialogInterface dialog, int which) {
+              shareApplicationByShareit();
+
+          }
+      }).create().show();
+
+    }
+
+    private void shareApplicationByShareit() {
+        PackageManager pm = getApplicationContext().getPackageManager();
+        ApplicationInfo appInfo;
+        try{
+             appInfo = pm.getApplicationInfo(getApplicationContext().getPackageName(),PackageManager.GET_META_DATA);
+             Intent sendBt = new Intent(Intent.ACTION_SEND);
+             sendBt.setType("*/*");
+            sendBt.putExtra(Intent.EXTRA_STREAM, Uri.fromFile(new File(appInfo.publicSourceDir)));
+            sendBt.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            startActivity(Intent.createChooser(sendBt, "Share it using"));
+
+        }catch (PackageManager.NameNotFoundException e1){
+            e1.printStackTrace();
+        }
+    }
+
+    private void shareApplicationByBluetooth() {
+        try {
+            ApplicationInfo app = getApplicationContext().getApplicationInfo();
+            String filepath = app.sourceDir;
+            Intent intent = new Intent(Intent.ACTION_SEND);
+            intent.setType("*/*");
+            intent.setPackage("com.android.bluetooth");
+            intent.putExtra(Intent.EXTRA_STREAM, Uri.fromFile(new File(filepath)));
+            startActivity(Intent.createChooser(intent,"Share app"));
+            Toast.makeText(getApplicationContext(),"Share the Programming quiz by bluetooth ", Toast.LENGTH_LONG).show();
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+    }
+
+    private void initViews(){
+        //signin_img = (ImageView) findViewById(R.id.header_signin);
+        username_txtview = (TextView) findViewById(R.id.userName_txtview);
+        profile_imgView = (ImageView) findViewById(R.id.imageView_profile);
+        imageView_navsend =(ImageView)findViewById(R.id.nav_send);
+        imageView_navNotification =(ImageView)findViewById(R.id.nav_notification);
+
+    }
+    private void showContactUsDialog() {
+        new AlertDialog.Builder(this)
+                .setMessage("For any queries contact this email \n sainathhiwale5@gmail.com")
+                .setPositiveButton("send email", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                        startActivity(new Intent(Intent.ACTION_SENDTO, Uri.parse("mailto:sainathhiwale5@gmail.com")));
+                    }
+                }).create().show();
+
+           }
+
+
+
+
+
 
 }
+
