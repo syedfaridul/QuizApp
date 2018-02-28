72129963dc9229a49287c14eb5e16ca9a6b36a7c
Sainathhiwale <sainathhiwale2@gmail.com>
2018-02-28 14:29:02 +0530

    test updated

--- app/src/main/java/com/dorvis/quizapp/fragments/ToDoInterviewFragment.java
+++ app/src/main/java/com/dorvis/quizapp/fragments/ToDoInterviewFragment.java
@@ -9,6 +9,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.dorvis.quizapp.ExperienceActivity;
 import com.dorvis.quizapp.FresherActivity;
@@ -28,6 +29,7 @@ public class ToDoInterviewFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_to_do_interview,container,false);
         Button fresher_bt = (Button)view.findViewById(R.id.fresher_button);
         Button experience_bt =(Button)view.findViewById(R.id.experience_button);
+        Button technical_bt = (Button)view.findViewById(R.id.technical_button);
 
         fresher_bt.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -44,6 +46,12 @@ public class ToDoInterviewFragment extends Fragment {
 
               Intent intent = new Intent(getActivity().getApplicationContext(), ExperienceActivity.class);
               startActivity(intent);
+            }
+        });
+        technical_bt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getActivity().getApplicationContext(),"Coming soon....",Toast.LENGTH_SHORT).show();
             }
         });
         return view;
--- app/src/main/java/com/dorvis/quizapp/fragments/ToDoNotificationFragment.java
+++ app/src/main/java/com/dorvis/quizapp/fragments/ToDoNotificationFragment.java
@@ -6,6 +6,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import com.dorvis.quizapp.R;
 
@@ -13,7 +14,8 @@ import com.dorvis.quizapp.R;
  * A simple {@link Fragment} subclass.
  */
 public class ToDoNotificationFragment extends Fragment {
-
+private ImageView notfiy_imageview;
+private ImageView iv_FAQ;
 
     public ToDoNotificationFragment() {
         // Required empty public constructor
@@ -23,8 +25,15 @@ public class ToDoNotificationFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_to_do_notification, container, false);
+       View  view= inflater.inflate(R.layout.fragment_to_do_notification, container, false);
+       notfiy_imageview = (ImageView)view.findViewById(R.id.notify_imageview);
+       iv_FAQ =(ImageView)view.findViewById(R.id.faq_back_arrow);
+
+
+        return view;
     }
 
 }
