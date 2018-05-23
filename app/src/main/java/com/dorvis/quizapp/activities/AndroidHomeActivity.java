5a870d3ae62df69c12de39868ed7e21fa33810eb
Sainathhiwale <sainathhiwale2@gmail.com>
2018-05-24 01:13:44 +0530

    android home activity added

---
+++ app/src/main/java/com/dorvis/quizapp/activities/AndroidHomeActivity.java
@@ -0,0 +1,15 @@
+package com.dorvis.quizapp.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.dorvis.quizapp.R;
+
+public class AndroidHomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_android_home);
+    }
+}
