13a46a75ccb6c4161cc57e752413fd2bff89a7a4
Sainathhiwale <sainathhiwale2@gmail.com>
2018-06-10 11:18:33 +0530

    android home page test updated

--- app/src/main/java/com/dorvis/quizapp/activities/AndroidHomesActivity.java
+++ app/src/main/java/com/dorvis/quizapp/activities/AndroidHomesActivity.java
@@ -1,6 +1,9 @@
 package com.dorvis.quizapp.activities;
 
 import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -8,6 +11,12 @@ import android.support.v7.widget.Toolbar;
 import android.widget.TableLayout;
 
 import com.dorvis.quizapp.R;
+import com.dorvis.quizapp.fragments.AndroidProjectFragment;
+import com.dorvis.quizapp.fragments.AndroidQuizFragment;
+import com.dorvis.quizapp.fragments.AndroidTutorialFragment;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class AndroidHomesActivity extends AppCompatActivity {
      private Toolbar toolbar;
@@ -33,12 +42,38 @@ public class AndroidHomesActivity extends AppCompatActivity {
 
     private void setupViewPager(ViewPager viewPager) {
         ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
-        adapter.addFragment(new OneFragment(), "ONE");
-        adapter.addFragment(new TwoFragment(), "TWO");
-        adapter.addFragment(new ThreeFragment(), "THREE");
+        adapter.addFragment(new AndroidTutorialFragment(), "Tutorial");
+        adapter.addFragment(new AndroidQuizFragment(), "Quiz");
+        adapter.addFragment(new AndroidProjectFragment(), "Project");
         viewPager.setAdapter(adapter);
     }
 
-    class ViewPagerAdapter
+    class ViewPagerAdapter extends FragmentPagerAdapter {
+        private final List<Fragment> mFragmentList = new ArrayList<>();
+        private final List<String> mFragmentTitleList = new ArrayList<>();
 
+        public ViewPagerAdapter(FragmentManager manager) {
+            super(manager);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return mFragmentList.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return mFragmentList.size();
+        }
+
+        public void addFragment(Fragment fragment, String title) {
+            mFragmentList.add(fragment);
+            mFragmentTitleList.add(title);
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return mFragmentTitleList.get(position);
+        }
+    }
 }
